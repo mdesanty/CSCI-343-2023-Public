e9fa13380c3abbdd425d1df91e95a946c25afbac
Michael DeSanty <michael.desanty@gmail.com>
2023-02-03 17:47:55 -0500

    Lecturcize15 - Intro to Promises

---
+++ Lecturcize15 - Introduction to Promises/server.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+app.get("/add", add);
+
+const listener = app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(`Server listening at ${listener.address().address}:${listener.address().port}`);
+});
+
+function add(req, res) {
+  executeAdd(req.query.a, req.query.b)
+    .then((result) => { res.json({ result: result.sum }) })
+    .catch((error) => { res.status(400).json({ error: error.message }) });
+}
+
+function executeAdd(a, b) {
+  const additionPromise = new Promise((resolve, reject) => {
+    if (a === undefined || b === undefined)
+      reject(new Error('Both a and b are required.'));
+
+    if (isNaN(a) || isNaN(b))
+      reject(new Error('Both a and b must be numbers.'));
+
+    const sum = parseFloat(a) + parseFloat(b);
+    resolve({ sum });
+  });
+
+  return additionPromise
+}
